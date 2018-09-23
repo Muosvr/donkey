ba74bc96edf89bc39b5e4b4ac05d621e4affc433
Muosvr <lujason2015@gmail.com>
2018-09-23 17:36:36 -0400

    serial_controller.py update

--- donkeycar/parts/serial_controller.py
+++ donkeycar/parts/serial_controller.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""
+Scrits to read signals from Arduino and convert into steering and angle outputs
+Arduino input signal range: 0 to 200
+Output range: -1.00 to 1.00
+"""
 
 import serial
+import time
 
 class SerialController:
     def __init__(self):
@@ -10,30 +16,21 @@ class SerialController:
         self.throttle = 0.0
         self.mode = 'user'
         self.recording = False
+        self.serial = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #Serial port - laptop: 'COM3', Arduino: '/dev/ttyACM0'
 
     def update(self):
-        print("Serial controller running")
-        with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
-            while(True):
-                line = str(ser.readline())[2:-5]
-                output = line.split(" ")
-                if output != ['']:
-                    self.angle = float(output[0])
-                    self.throttle = float(output[1])
-                    # print(self.angle, self.throttle)
+        while True:
+            line = str(self.serial.readline())[2:-5]
+            output = line.split(" ")
+            if len(output) == 2:
+                if output[0].isnumeric() and output[1].isnumeric():
+                    self.angle = (float(output[0])-100)/100
+                    self.throttle = (float(output[1])-100)/100
+                    time.sleep(0.01)
+
+    def run(self):
+        return self.run_threaded()
 
     def run_threaded(self):
-        return self.angle, self.throttle
-
-
-carPart = SerialController()
-carPart.update()
-#Serial port - laptop: 'COM3', Arduino: '/dev/ttyACM0'
-# with serial.Serial('COM3', 9600, timeout=1) as ser:
-#     while(True):
-#         line = str(ser.readline())[2:-5]
-#         output = line.split(" ")
-#         if output != ['']:
-#             steering = float(output[0])
-#             throttle = float(output[1])
-#             return steering, throttle
+        #print("Signal:", self.angle, self.throttle)
+        return self.angle, self.throttle, self.mode, self.recording
